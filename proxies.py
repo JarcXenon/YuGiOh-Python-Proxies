de071aad1391eb32ed186800bb5f74c94775702d
Leon Frenot <leon.frenot@gmail.com>
2020-03-31 23:30:42 +0200

    Revert "Correction cas ' + ajout decklist + fichiers en trop retirés"
    This reverts commit b5e4ca21115ecde7a12785a15cf48b35ad0f0bfa.

--- proxies.py
+++ proxies.py
@@ -7,7 +7,7 @@ def NameToCode(nom):
     debut = txt.find('id') + 4
     fin = txt.find(',"name"')
     return(txt[debut:fin])
-
+    
 if not os.path.isfile('decklist.txt') and os.path.isfile('decklist.ydk'):
     data = requests.get('https://db.ygoprodeck.com/api/v6/cardinfo.php').content
     txt = str(data)
@@ -15,21 +15,21 @@ if not os.path.isfile('decklist.txt') and os.path.isfile('decklist.ydk'):
         with open('decklist.txt', 'w') as g:
             lignes = f.readlines()
             for i in lignes:
-                if i[0] != '#' and i[0] != '!' and i != '\n':
+                if i[0] != '#' and i != '\n':
                     code = i[:-1]
                     debut = txt.find(code)
                     texte = txt[debut:]
                     fin = texte.find(',"type') - 1
                     debut = texte.find('name"') + 7
                     nom = texte[debut:fin]
-                    g.write(nom.replace("\\", "") + '\n')
-
+                    g.write(nom + '\n')
+                    
 with open('decklist.txt', 'r') as f:
     with open('cards.tex', 'w') as g:
         lignes = f.readlines()
         i = 0
         for ligne in lignes:
-            if ligne[0] != '#' and ligne[0] != '!' and ligne != '\n':
+            if ligne[0] != '#' and ligne != '\n':
                 g.write('\\'+'card{' + ligne[:-1].replace(' ','_').replace('/','_') + '}\n')
                 i += 1
                 if i == 3:
@@ -43,5 +43,5 @@ with open('decklist.txt', 'r') as f:
                     data = requests.get(lien).content
                     with open('images/'+ jpg, 'wb') as handle:
                         handle.write(data)
-
+                    
 os.system('pdflatex proxies.tex')
